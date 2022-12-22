599bebd0ac213f23dcac271822c771d34b09aa57
AkashKumar-Akki <dakash5539gmail.com>
2022-12-22 15:06:05 +0530

    first

--- demo.py
+++ demo.py
@@ -1,9 +1,9 @@
+print("hi")
 from flask import Flask,render_template
 app=Flask(__name__)
 
 @app.route('/')
 def demo():
         return render_template('Home.html')
-
-if__name__=='__main__':
-    app.run();
+if __name__ == '__main__':
+        app.run();
